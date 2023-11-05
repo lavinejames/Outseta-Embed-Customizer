f45e169492a69fcee869f3dfb1bdb30d4d841c00
lavinejames <hello@jameslavine.com>
2023-11-05 12:49:10 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 // Sync input ranges with the input numbers
-document.querySelector(".input-range").addEventListener("input", updateValue);
-document.querySelector(".input-number").addEventListener("input", updateValue);
+const inputRangeElements = document.querySelectorAll(".input-range");
+
+inputRangeElements.forEach(function(element) {
+    element.addEventListener("input", updateValue);
+});
+
+const inputNumberElements = document.querySelectorAll(".input-number");
+
+inputNumberElements.forEach(function(element) {
+    element.addEventListener("input", updateValue);
+});
 
 function updateValue(e) {
   var sibling = e.target.previousElementSibling || e.target.nextElementSibling;
@@ -34,6 +43,17 @@ wrapperRadius.addEventListener("input", function () {
   );
 });
 
+// Wrapper padding
+const wrapperPadding = document.getElementById("wrapperPadding");
+
+wrapperPadding.addEventListener("input", function () {
+  const newWrapperPadding = wrapperPadding.value;
+  document.documentElement.style.setProperty(
+    "--embed--wrapper-padding",
+    newWrapperPadding + "px"
+  );
+});
+
 // Input radius
 const inputRadius = document.getElementById("inputRadius");
 
@@ -54,4 +74,20 @@ buttonRadius.addEventListener("input", function () {
     "--embed--button-radius",
     newButtonRadius + "px"
   );
+});
+
+// Dark mode
+const darkMode = document.getElementById('darkMode');
+
+// Add an event listener for the 'change' event
+darkMode.addEventListener('change', function () {
+  if (darkMode.checked) {
+    // Checkbox is checked, update CSS variables
+    document.documentElement.style.setProperty('--embed-wrapper-bg', 'var(--neutral--900)');
+    document.documentElement.style.setProperty('--embed-text-color', 'var(--neutral--white)');
+  } else {
+    // Checkbox is unchecked, set CSS variables to their initial values
+    document.documentElement.style.setProperty('--embed-wrapper-bg', 'var(--neutral--white)');
+    document.documentElement.style.setProperty('--embed-text-color', 'var(--neutral--900)');
+  }
 });
